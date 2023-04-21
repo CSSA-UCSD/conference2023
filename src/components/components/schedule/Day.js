a271aabf23a370ec1f074c47a31c88b8629bc5bb
Vicky <v2chou@ucsd.edu>
2023-04-21 14:32:33 -0700

    refactored day.js to functional component and populated schedule page with day schedule data

--- src/components/components/schedule/Day.js
+++ src/components/components/schedule/Day.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 // import { Link } from 'react-router-dom';
 import { HashLink as Link } from 'react-router-hash-link';
 
@@ -6,66 +6,62 @@ import AOS from "aos";
 import StickyBox from "react-sticky-box";
 
 import Event from "../../components/schedule/Event";
-import EventData from "../../data/event/EventData";
+import DayScheduleData from "../../data/home/DayScheduleData";
 
-class Day extends React.Component {
-
-    componentDidMount() {
+function Day() {
+    // componentDidMount() {
+    //     AOS.init({
+    //         duration : 2000
+    //     });
+    // }
+    useEffect(() => {
         AOS.init({
-            duration : 2000
+            duration: 2000
         });
+    })
+
+    const getWeekday = (date, length) => {
+        return new Intl.DateTimeFormat("en-US", { weekday: length }).format(new Date(date));
     }
 
-    render() {
-        const Day1Component = EventData.filter(item => item.day === 1).map(item => < Event key={item.id} item={item}/>);
-        const Day2Component = EventData.filter(item => item.day === 2).map(item => < Event key={item.id} item={item}/>);
-        const Day3Component = EventData.filter(item => item.day === 3).map(item => < Event key={item.id} item={item}/>);
 
-        return (
-            <div className="day-schedule pb-5" data-aos="fade-up">
-                <div className="row" style={{ display: 'flex', alignItems: 'flex-start' }}>
-                    <StickyBox offsetBottom={40} className="sidebar mx-auto" >
-                        {/* <div className=""> */}
-                            <ul>
-                                <li>
-                                    <Link smooth to="#day1">Day 1</Link>
-                                </li>
-                                <li>
-                                    <Link smooth to="#day2">Day 2</Link>
-                                </li>
-                                <li>
-                                    <Link smooth to="#day3">Day 3</Link>
-                                </li>
-                            </ul>
-                        {/* </div> */}
-                    </StickyBox>
-                    <div className="content">
-                        <div className="day" id="day1">
+    return (
+        <div className="day-schedule pb-5" data-aos="fade-up">
+            <div className="row" style={{ display: 'flex', alignItems: 'flex-start' }}>
+                <StickyBox offsetBottom={40} className="sidebar mx-auto" >
+                    {/* <div className=""> */}
+                    <ul>
+                        <li>
+                            <Link smooth to="#day1">Day 1</Link>
+                        </li>
+                        <li>
+                            <Link smooth to="#day2">Day 2</Link>
+                        </li>
+                        <li>
+                            <Link smooth to="#day3">Day 3</Link>
+                        </li>
+                    </ul>
+                    {/* </div> */}
+                </StickyBox>
+                <div className="content">
+                    {DayScheduleData.map(day => {
+                        return <div className="day" key={day.id}>
                             <div className="schedule-title header-gradient">
-                                <h1>Day 1</h1>
+                                <h1>Day {day.id}</h1>
                             </div>
-                            {Day1Component}
+                            <h4>{getWeekday(day.date, "short")} {day.date} | {day.time}</h4>
+                            <span>📍{day.location}</span>
+                            {/* {Day1Component} */}
+                            {day.events.map((item, key) => {
+                                return < Event key={key} item={item} />
+                            })
+                            }
                         </div>
-                        <div className="day" id="day2">
-                            <div className="schedule-title header-gradient">
-                                <h1>Day 2</h1>
-                            </div>
-                            {/* <p>{this.props.item.description}</p> */}
-                            {Day2Component}
-                        </div>
-                        <div className="day" id="day3">
-                            <div className="schedule-title header-gradient">
-                                <h1>Day 3</h1>
-                            </div>
-                            {/* <p>{this.props.item.description}</p> */}
-                            {Day3Component}
-                        </div>
-                    </div>
+                    })}
                 </div>
-
             </div>
-        );
-    }
+        </div>
+    );
 }
 
 export default Day;

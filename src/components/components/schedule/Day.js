2bec1f26c2beb43ecbf3a509dd99e0fe85a7538a
Lucy Lennemann <llennema@ucsd.edu>
2023-04-23 14:12:25 -0700

    Working on fonts/colors/sizes for schedule pg

--- src/components/components/schedule/Day.js
+++ src/components/components/schedule/Day.js
@@ -52,9 +52,7 @@ function Day() {
                 <div className="content">
                     {DayScheduleData.map(day => {
                         return <div className="day" key={day.id}>
-                            <div className="schedule-title header-gradient">
-                                <h1>Day {day.id}</h1>
-                            </div>
+                            <h1>{day.name}</h1>
                             <h4>{getWeekday(day.date, "short")} {day.date} | {day.time}</h4>
                             <span>{day.location}</span>
                             {/* {Day1Component} */}

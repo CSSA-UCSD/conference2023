598ce7a64307c0e7f4d32894f3111fe295953645
Vicky <v2chou@ucsd.edu>
2023-04-26 04:32:26 -0700

    speaker image styling, fixed page background

--- src/App.js
+++ src/App.js
@@ -1,16 +1,12 @@
 import React from 'react';
 import './style/main.scss';
-import { useLocation } from "react-router-dom";
 import Navbar from './components/components/Navbar';
 import Main from './components/Main';
 import Footer from './components/components/Footer';
 
 function App() {
-  const path = useLocation().pathname;
-  const route = path.split("/")[1];
-  console.log(route)
   return (
-    <div className={"App " + route}>
+    <div className={"App"}>
       < Navbar/>
       < Main/>
       < Footer/>
--- src/components/pages/Speakers.js
+++ src/components/pages/Speakers.js
@@ -1,34 +1,41 @@
-import React from "react";
+import React, { useEffect } from "react";
 import AOS from "aos";
 
 import Speaker from "../components/speakers/Speaker"
 import SpeakerPreview from "../components/home/SpeakerPreview";
 import SpeakerData from "../data/SpeakerData";
+import DayScheduleData from "../data/home/DayScheduleData";
 
-class Events extends React.Component {
-    componentDidMount() {
+function Events() {
+
+    useEffect(() => {
+        document.body.classList.add('speakers');
         AOS.init({
-            duration : 2000
+            duration: 2000
         });
-    }
+    })
 
-    render() {
-        const SpeakerComponent = SpeakerData.map(item => < Speaker key={item.id} item={item}/>);
-
-        return (
-            <div className="speakers">
-                <div className="container py-5">
-                    <div className="speakers-title header-gradient pt-5">
-                        <h1 className="">Speakers</h1>
-                        <p className="px-5 mt-3">Soon to be released!</p>
-                    </div>
+    return (
+        // <div className="mx-5">
+            <div className="container py-5">
+                <div className="speakers-title header-gradient pt-5">
+                    <h1 className="">Speakers</h1>
+                    {/* <p className="px-5 mt-3">Soon to be released!</p> */}
                 </div>
-                {/* { <div className="px-5 ">
-                    {SpeakerComponent}
-                </div> } */}
-            </div>
-        );
-    }
+                { DayScheduleData.map((day) => {
+                    let daySpeakers = SpeakerData.filter(speaker => speaker.day === day.id);
+                    return <div className="mt-5" key={day.id}>
+                        <h1 className="mb-5">Day <span className="ital">0{day.id}</span></h1>
+                        <div className="row justify-content-center">
+                        {
+                            daySpeakers.map(item => <Speaker key={item.id} item={item} />)
+                        }
+                        </div>
+                    </div>
+                })}
+            </div>            
+        // </div>
+    )
 }
 
 export default Events;
